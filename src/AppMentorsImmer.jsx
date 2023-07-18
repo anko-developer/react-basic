e59922adc8f35057eddf52a2b4accd76e53ecac8
it1403 <anko@gsitm.com>
2023-07-18 16:52:40 +0900

    상태관리법

--- src/AppMentorsImmer.jsx
+++ src/AppMentorsImmer.jsx
@@ -1,25 +1,42 @@
-import React, { useReducer } from 'react';
-import personReducer from './reducer/person-reducer';
+import React from 'react';
+import { useImmer } from 'use-immer';
 
-export default function AppMentor() {
-  // const [person, setPerson] = useState(initialPerson);
-  const [person, dispatch] = useReducer(personReducer, initialPerson);
+export default function AppMentorImmer() {
+  const [person, updatePerson] = useImmer(initialPerson);
+
   const handleUpdate = () => {
     const prev = prompt('누구의 이름을 바꾸고 싶은가요?');
     const current = prompt('이름을 무엇으로 바꾸고 싶은가요?');
 
-    dispatch({type: 'updated', prev, current});
+    updatePerson(person => {
+      const mentor = person.mentors.find(m => m.name === prev);
+      if (!mentor) {
+        return;
+      }
+      mentor.name = current;
+    });
   };
   const handleAdd = () => {
     const name = prompt('추가하실 멘토의 이름?');
     const title = prompt('추가하실 멘토의 직업?');
     
-    dispatch({type: 'added', name, title});
+    updatePerson(person => {
+      person.mentors.push({
+        name,
+        title
+      });
+    });
   };
   const handleDelete = () => {
     const name = prompt('누구의 이름을 삭제하고 싶은가요?');
 
-    dispatch({type: 'deleted', name});
+    updatePerson(person => {
+      const index = person.mentors.findIndex(m => m.name === name);
+      if (index <= 0) {
+        return;
+      }
+      person.mentors.splice(index, 1);
+    })
   };
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppXY from './AppXY';
-import AppMentors from './AppMentors';
+import AppForm from './AppForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMentors />
+    <AppForm />
   </React.StrictMode>
 );
 
