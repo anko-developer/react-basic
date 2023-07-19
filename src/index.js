95984489078a082357ddea8792daefc42205f932
김명욱 <anko@gsitm.com>
2023-07-19 13:07:45 +0900

    useCallback, memo 파악

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppXY from './AppXY';
-import AppTheme from './AppTheme';
+import AppMentorsButton from './AppMentorsButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppTheme />
+    <AppMentorsButton />
   </React.StrictMode>
 );
 
