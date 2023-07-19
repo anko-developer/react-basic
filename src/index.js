379df6eae4ab465ad8d6e408868bd09ab7384630
김명욱 <anko@gsitm.com>
2023-07-19 12:38:48 +0900

    context

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppXY from './AppXY';
-import AppWrap from './AppWrap';
+import AppTheme from './AppTheme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppWrap />
+    <AppTheme />
   </React.StrictMode>
 );
 
